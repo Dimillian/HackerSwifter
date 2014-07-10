6dd7b5d0fe2e6732ed010c6c3934f1d948f6ab2b
Thomas Ricouard <ricouard77@gmail.com>
2014-07-10 22:44:07 +0200

    Initial framework

---
+++ Hacker Swifter/Hacker Swifter/Hacker Swifter.h
@@ -0,0 +1,19 @@
+//
+//  Hacker Swifter.h
+//  Hacker Swifter
+//
+//  Created by Thomas Ricouard on 10/07/14.
+//  Copyright (c) 2014 Thomas Ricouard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Hacker Swifter.
+FOUNDATION_EXPORT double Hacker_SwifterVersionNumber;
+
+//! Project version string for Hacker Swifter.
+FOUNDATION_EXPORT const unsigned char Hacker_SwifterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hacker_Swifter/PublicHeader.h>
+
+
