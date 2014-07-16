3069b31a37905d67f029e4522a619b46a7e6fa80
Thomas Ricouard <ricouard77@gmail.com>
2014-07-16 20:18:43 +0200

    Cache system

---
+++ Hacker Swifter/Hacker Swifter/HTTP/Cache.swift
@@ -0,0 +1,169 @@
+//
+//  Cache.swift
+//  HackerSwifter
+//
+//  Created by Thomas Ricouard on 16/07/14.
+//  Copyright (c) 2014 Thomas Ricouard. All rights reserved.
+//
+
+import Foundation
+
+let _Cache = Cache()
+let _MemoryCache = MemoryCache()
+let _DiskCache = DiskCache()
+
+typealias cacheCompletion = (AnyObject!) -> Void
+
+class Cache {
+
+    class var sharedCache: Cache {
+        return _Cache
+    }
+    
+    init() {
+        
+    }
+    
+    class func generateCacheKey(path: String) -> String {
+        return path.stringByReplacingOccurrencesOfString("/",
+            withString: "#", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
+    }
+    
+    func setObject(object: AnyObject, key: String) {
+        if (object.conformsToProtocol(NSCoding)) {
+            MemoryCache.sharedMemoryCache.setObject(object, key: key)
+            DiskCache.sharedDiskCache.setObject(object, key: key)
+        }
+    }
+    
+    func objectForKey(key: String, completion: cacheCompletion) {
+        MemoryCache.sharedMemoryCache.objectForKey(key, completion: {(object: AnyObject!) in
+            if let realObject: AnyObject = object {
+                completion(realObject)
+            }
+            else {
+                DiskCache.sharedDiskCache.objectForKey(key, completion: {(object: AnyObject!) in
+                    completion(object)
+                })
+            }
+        })
+    }
+    
+    func objectForKeySync(key: String) -> AnyObject! {
+        var ramObject: AnyObject! = MemoryCache.sharedMemoryCache.objectForKeySync(key)
+        return ramObject ? ramObject : DiskCache.sharedDiskCache.objectForKeySync(key)
+    }
+    
+    func removeObject(key: String) {
+        MemoryCache.sharedMemoryCache.removeObject(key)
+        DiskCache.sharedDiskCache.removeObject(key)
+    }
+    
+    func removeAllObject() {
+        MemoryCache.sharedMemoryCache.removeAllObject()
+        DiskCache.sharedDiskCache.removeAllObject()
+    }
+}
+
+class DiskCache: Cache {
+    
+    struct files {
+        static var filepath: String {
+            var manager = NSFileManager.defaultManager()
+            var paths = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.CachesDirectory,
+                NSSearchPathDomainMask.UserDomainMask, true)
+            var cachePath = paths[0] as String + "/modelCache/"
+                if (!manager.fileExistsAtPath(cachePath)) {
+                    manager.createDirectoryAtPath(cachePath, withIntermediateDirectories: true, attributes: nil, error: nil)
+                }
+            return cachePath
+        }
+    }
+    
+    let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
+
+    class var sharedDiskCache: Cache {
+        return _DiskCache
+    }
+
+    init() {
+
+    }
+    
+    func fullPath(key: String) -> String {
+        return files.filepath + key
+    }
+    
+    func objectExist(key: String) -> Bool {
+        return NSFileManager.defaultManager().fileExistsAtPath(fullPath(key))
+    }
+    
+    override func objectForKey(key: String, completion: cacheCompletion) {
+        if (self.objectExist(key)) {
+            dispatch_async(dispatch_get_global_queue(self.priority, UInt(0)), { ()->() in
+                var object: AnyObject! =  NSKeyedUnarchiver.unarchiveObjectWithFile(self.fullPath(key))
+                dispatch_async(dispatch_get_main_queue(), { ()->() in
+                    completion(object)
+                    })
+                })
+        }
+        else {
+            completion(nil)
+        }
+    }
+    
+    override func objectForKeySync(key: String) -> AnyObject! {
+        if (self.objectExist(key)) {
+            return NSKeyedUnarchiver.unarchiveObjectWithFile(self.fullPath(key))
+        }
+        return nil
+    }
+    
+    override func setObject(object: AnyObject, key: String) {
+        NSKeyedArchiver.archiveRootObject(object, toFile: self.fullPath(key))
+    }
+    
+    override func removeObject(key: String) {
+        if (self.objectExist(key)) {
+            NSFileManager.defaultManager().removeItemAtPath(self.fullPath(key), error: nil)
+        }
+    }
+    
+    override func removeAllObject() {
+
+    }
+}
+
+class MemoryCache: Cache {
+    
+    var memoryCache = NSCache()
+    
+    class var sharedMemoryCache: Cache {
+        return _MemoryCache
+    }
+    
+    init() {
+        
+    }
+    
+    override func objectForKeySync(key: String) -> AnyObject! {
+        return self.memoryCache.objectForKey(key)
+    }
+    
+    override func objectForKey(key: String, completion: cacheCompletion)  {
+        completion(self.memoryCache.objectForKey(key))
+    }
+    
+    override func setObject(object: AnyObject, key: String) {
+        self.memoryCache.setObject(object, forKey: key)
+    }
+    
+    override func removeObject(key: String) {
+        self.memoryCache.removeObjectForKey(key)
+    }
+    
+    override func removeAllObject() {
+        self.memoryCache.removeAllObjects()
+    }
+    
+}
---
+++ Hacker Swifter/Hacker SwifterTests/CacheTests.swift
@@ -0,0 +1,94 @@
+//
+//  CacheTests.swift
+//  HackerSwifter
+//
+//  Created by Thomas Ricouard on 16/07/14.
+//  Copyright (c) 2014 Thomas Ricouard. All rights reserved.
+//
+
+import XCTest
+import HackerSwifter
+
+class CacheTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    func pathtest() {
+        var path = "test/test?test/test"
+        var result = "test#test?test#test"
+        
+        XCTAssertTrue(DiskCache.generateCacheKey(path) == result, "cache key is not equal to result")
+    }
+    
+    func testMemoryCache() {
+        var post = Post()
+        post.title = "Test"
+        
+        MemoryCache.sharedMemoryCache.setObject(post, key: "post")
+        
+        var postTest = MemoryCache.sharedMemoryCache.objectForKeySync("post") as Post
+    
+        XCTAssertNotNil(postTest, "Post is nil")
+        XCTAssertTrue(postTest.isKindOfClass(Post), "Post is not kind of class post")
+        XCTAssertTrue(postTest.title == "Test", "Post title is not equal to prior test")
+        
+        MemoryCache.sharedMemoryCache.removeObject("post")
+        
+        XCTAssertNil(MemoryCache.sharedMemoryCache.objectForKeySync("post"), "post should be nil")
+    }
+    
+    func testDiskCache() {
+        var post = Post()
+        post.title = "Test"
+        
+        DiskCache.sharedDiskCache.setObject(post, key: "post")
+        
+        var postTest = DiskCache.sharedDiskCache.objectForKeySync("post") as Post
+        
+        XCTAssertNotNil(postTest, "Post is nil")
+        XCTAssertTrue(postTest.isKindOfClass(Post), "Post is not kind of class post")
+        XCTAssertTrue(postTest.title == "Test", "Post title is not equal to prior test")
+        
+        DiskCache.sharedDiskCache.removeObject("post")
+        
+        XCTAssertNil(DiskCache.sharedDiskCache.objectForKeySync("post"), "post should be nil")
+    }
+    
+    func testGlobalCache() {
+        var post = Post()
+        post.title = "Global Test"
+        
+        Cache.sharedCache.setObject(post, key: "post")
+        
+        var globalPost = Cache.sharedCache.objectForKeySync("post") as Post
+        var memoryPost = MemoryCache.sharedMemoryCache.objectForKeySync("post") as Post
+        var diskPost = DiskCache.sharedDiskCache.objectForKeySync("post") as Post
+        
+        XCTAssertNotNil(globalPost, "Global Post is nil")
+        XCTAssertNotNil(memoryPost, "Memory Post is nil")
+        XCTAssertNotNil(diskPost, "Dissk Post is nil")
+        
+        XCTAssertTrue(globalPost.isKindOfClass(Post), "Global Post is not kind of class post")
+        XCTAssertTrue(globalPost.title == "Global Test", "Global Post title is not equal to prior test")
+        
+        XCTAssertTrue(memoryPost.isKindOfClass(Post), "Memory Post is not kind of class post")
+        XCTAssertTrue(memoryPost.title == "Global Test", "Memory Post title is not equal to prior test")
+        
+        XCTAssertTrue(diskPost.isKindOfClass(Post), "Disk Post is not kind of class post")
+        XCTAssertTrue(diskPost.title == "Global Test", "Disk Post title is not equal to prior test")
+        
+        Cache.sharedCache.removeObject("post")
+        
+        XCTAssertNil(Cache.sharedCache.objectForKeySync("post"), "post should be nil")
+        XCTAssertNil(MemoryCache.sharedMemoryCache.objectForKeySync("post"), "post should be nil")
+        XCTAssertNil(DiskCache.sharedDiskCache.objectForKeySync("post"), "post should be nil")
+    }
+    
+}
