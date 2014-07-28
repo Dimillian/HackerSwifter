46b0cbb237639225659d33a5091d7818c0c5f706
Joseph Neuman <finder39@gmail.com>
2014-07-28 11:39:55 -0500

    Support for pages for filter and user

--- Hacker Swifter/Hacker Swifter/Models/Post.swift
+++ Hacker Swifter/Hacker Swifter/Models/Post.swift
@@ -92,46 +92,54 @@ public extension Post {
     
     public typealias Response = (posts: [Post]!, error: Fetcher.ResponseError!, local: Bool) -> Void
     public typealias ResponsePost = (post: Post!, error: Fetcher.ResponseError!, local: Bool) -> Void
-    
+  
+  public class func fetch(filter: PostFilter, page: Int, completion: Response) {
+    Fetcher.Fetch(filter.toRaw() + "?p=\(page)",
+      parsing: {(html) in
+        if let realHtml = html {
+          var posts = self.parseCollectionHTML(realHtml)
+          return posts
+        }
+        else {
+          return nil
+        }
+      },
+      completion: {(object, error, local) in
+        if let realObject: AnyObject = object {
+          completion(posts: realObject as [Post], error: error, local: local)
+        }
+        else {
+          completion(posts: nil, error: error, local: local)
+        }
+      })
+  }
+  
     public class func fetch(filter: PostFilter, completion: Response) {
-        Fetcher.Fetch(filter.toRaw(),
-            parsing: {(html) in
-                if let realHtml = html {
-                    var posts = self.parseCollectionHTML(realHtml)
-                    return posts
-                }
-                else {
-                    return nil
-                }
-            },
-            completion: {(object, error, local) in
-                if let realObject: AnyObject = object {
-                    completion(posts: realObject as [Post], error: error, local: local)
-                }
-                else {
-                    completion(posts: nil, error: error, local: local)
-                }
-            })
+      fetch(filter, page: 1, completion: completion)
     }
-    
+  
+  public class func fetch(user: String, page: Int, completion: Response) {
+    Fetcher.Fetch("submitted?id=" + user + "?p=\(page)", parsing: {(html) in
+      if let realHtml = html {
+        var posts = self.parseCollectionHTML(realHtml)
+        return posts
+      }
+      else {
+        return nil
+      }
+      },
+      completion: {(object, error, local) in
+        if let realObject: AnyObject = object {
+          completion(posts: realObject as [Post], error: error, local: local)
+        }
+        else {
+          completion(posts: nil, error: error, local: local)
+        }
+      })
+  }
+  
     public class func fetch(user: String, completion: Response) {
-        Fetcher.Fetch("submitted?id=" + user, parsing: {(html) in
-            if let realHtml = html {
-                var posts = self.parseCollectionHTML(realHtml)
-                return posts
-            }
-            else {
-                return nil
-            }
-        },
-        completion: {(object, error, local) in
-            if let realObject: AnyObject = object {
-                completion(posts: realObject as [Post], error: error, local: local)
-            }
-            else {
-                completion(posts: nil, error: error, local: local)
-            }
-        })
+      fetch(user, page: 1, completion: completion)
     }
     
     //Test using Algolia API For later
