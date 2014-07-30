7ddcf1cca420ab674bca0c33c4d60d56117d1e2e
Joseph Neuman <finder39@gmail.com>
2014-07-30 13:56:18 -0500

    Add Reachability to Fetcher

--- Hacker Swifter/Hacker Swifter/HTTP/Fetcher.swift
+++ Hacker Swifter/Hacker Swifter/HTTP/Fetcher.swift
@@ -16,6 +16,9 @@ public class Fetcher {
     internal let baseURL = "https://news.ycombinator.com/"
     internal let APIURL = "https://hn.algolia.io/api/v1/"
     private let session = NSURLSession.sharedSession()
+  
+    private let reachability: Reachability?
+  
     
     public typealias FetchCompletion = (object: AnyObject!, error: ResponseError!, local: Bool) -> Void
     public typealias FetchParsing = (html: String!) -> AnyObject!
@@ -26,9 +29,18 @@ public class Fetcher {
         case ErrorParsing = "An error occurred while fetching the requested page"
         case UnknownError = "An unknown error occurred"
     }
-    
+  
     public class var sharedInstance: Fetcher {
         return _Fetcher
+    }
+  
+    init() {
+        reachability = Reachability.reachabilityForInternetConnection()
+        reachability!.startNotifier()
+    }
+  
+    private func hostReachable() -> Bool {
+        return (_Fetcher.reachability!.currentReachabilityStatus() == NetworkStatus.ReachableViaWiFi || _Fetcher.reachability!.currentReachabilityStatus() == NetworkStatus.ReachableViaWWAN)
     }
     
     class func Fetch(ressource: String, parsing: FetchParsing, completion: FetchCompletion) {
@@ -36,39 +48,41 @@ public class Fetcher {
         self.showLoadingIndicator(true)
         
         var cacheKey = Cache.generateCacheKey(ressource)
-        Cache.sharedCache.objectForKey(cacheKey, completion: {(object: AnyObject!) in
-            if var realObject: AnyObject = object {
-                completion(object: realObject, error: nil, local: true)
-            }
-        })
-        
-        var path = _Fetcher.baseURL + ressource
-        var task = _Fetcher.session.dataTaskWithURL(NSURL(string: path) , completionHandler: {(data: NSData!, response, error: NSError!) in
-            if !error {
-                if let realData = data {
-                    var object: AnyObject! = parsing(html: NSString(data: realData, encoding: NSUTF8StringEncoding))
-                    if var realObject: AnyObject = object {
-                        Cache.sharedCache.setObject(realObject, key: cacheKey)
-                    }
-                    dispatch_async(dispatch_get_main_queue(), { ()->() in
-                        self.showLoadingIndicator(false)
-                        completion(object: object, error: nil, local: false)
+        if (_Fetcher.hostReachable()) {
+            var path = _Fetcher.baseURL + ressource
+            var task = _Fetcher.session.dataTaskWithURL(NSURL(string: path) , completionHandler: {(data: NSData!, response, error: NSError!) in
+                if !error {
+                    if let realData = data {
+                        var object: AnyObject! = parsing(html: NSString(data: realData, encoding: NSUTF8StringEncoding))
+                        if var realObject: AnyObject = object {
+                            Cache.sharedCache.setObject(realObject, key: cacheKey)
+                        }
+                        dispatch_async(dispatch_get_main_queue(), { ()->() in
+                            self.showLoadingIndicator(false)
+                            completion(object: object, error: nil, local: false)
                         })
+                    }
+                    else {
+                        dispatch_async(dispatch_get_main_queue(), { ()->() in
+                            self.showLoadingIndicator(false)
+                            completion(object: nil, error: ResponseError.UnknownError, local: false)
+                        })
+                    }
                 }
                 else {
-                    dispatch_async(dispatch_get_main_queue(), { ()->() in
-                        self.showLoadingIndicator(false)
-                        completion(object: nil, error: ResponseError.UnknownError, local: false)
-                        })
+                    completion(object: nil, error: ResponseError.UnknownError, local: false)
                 }
-            }
-            else {
-                completion(object: nil, error: ResponseError.UnknownError, local: false)
-            }
-        })
-        task.resume()
+            })
+            task.resume()
+        } else {
+            Cache.sharedCache.objectForKey(cacheKey, completion: {(object: AnyObject!) in
+                if var realObject: AnyObject = object {
+                    completion(object: realObject, error: nil, local: true)
+                }
+            })
+        }
     }
-    
+  
     //In the future, all scraping will be removed and we'll use only the Algolia API
     //At the moment this function is sufixed for testing purpose
     class func FetchAPI(ressource: String, parsing: FetchParsingAPI, completion: FetchCompletion) {
